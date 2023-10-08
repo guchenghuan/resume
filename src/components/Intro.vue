2cf62858daa3c002545adfd12411dde941fdef2b
guchenghuan <guchenghuan@xiaohongshu.com>
2023-10-08 17:36:16 +0800

    feat: my intro

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -4,16 +4,22 @@
       <div class="cover-main">
         <div class="cover-main-left">
           <p>
-            非科班出身，大学期间学过C++，后来18年大学毕业，开始接触编程，颇感兴趣。于是独立自学
-            Web 开发技术。入坑后，成为半疯狂的技术追求者。
+            非科班出身，大学期间学过
+            <strong>C++、JAVA</strong> 。18年大学毕业后，开始接触
+            <strong>JavaScript</strong> ，很感兴趣。于是自学
+            Web相关的技术。之后对前端的热爱一发不可收拾
           </p>
           <p style="margin-top: 10px">
-            拥有丰富的测试工具开发经验，前端开发经验。担任测试工具组的Owner，QA前端组的Owner
+            拥有丰富的测试工具开发经验，前端开发经验。担任过
+            <strong>测试工具组的Owner</strong> ，<strong
+              >QA前端组的Owner</strong
+            >
           </p>
           <p style="margin-top: 10px">我可以使用它们来创造工作中需要的一切：</p>
           <ul>
             <li>JavaScript</li>
             <li>TypeScript</li>
+            <li>JAVA</li>
           </ul>
           <p style="margin-top: 10px">我可以使用它们来十分出色的完成工作：</p>
           <p><strong>技术：</strong></p>
@@ -23,8 +29,8 @@
           <p style="margin-top: 10px">
             <strong>业务倾向：</strong>
           </p>
-          测试业务方向的应用设计开发、复杂的应用开发，落地到实际工作使用中
-          对业务有实际的正向反馈，同时它也能被赋予 <strong>'意义'</strong>
+          技术在为业务服务的同时，它也要被赋予
+          <strong>'意义'</strong>
         </div>
         <div class="cover-img" id="main"></div>
       </div>
@@ -59,8 +65,8 @@ export default {
             { name: "Vue", max: 100 },
             { name: "React", max: 100 },
             { name: "Node.js", max: 100 },
-            { name: "ES6", max: 80 },
-            { name: "Golang", max: 100 },
+            { name: "ES6", max: 100 },
+            { name: "JAVA", max: 100 },
           ],
           axisName: {
             formatter: "{value}",
@@ -73,7 +79,7 @@ export default {
             type: "radar",
             data: [
               {
-                value: [95, 90, 80, 95, 90, 70, 80, 50],
+                value: [95, 90, 80, 95, 90, 90, 80, 70],
                 name: "Score",
                 areaStyle: {
                   color: new this.$echarts.graphic.RadialGradient(0.1, 0.6, 1, [
